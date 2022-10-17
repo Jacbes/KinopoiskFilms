d012d6035025d807d0156d7cba062ddaec9596d9
Jacbes <skishon100000@gmail.com>
2022-10-17 20:18:37 +0700

    Add PostgreSQL sample

--- src/main/java/dev/jabes/kinopoiskfilms/KinopoiskFilmsApplication.java
+++ src/main/java/dev/jabes/kinopoiskfilms/KinopoiskFilmsApplication.java
@@ -1,13 +1,25 @@
 package dev.jabes.kinopoiskfilms;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KinopoiskFilmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KinopoiskFilmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KinopoiskFilmsApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner commandLineRunner(MovieRepository movieRepository) {
+        return args -> {
+            Movie brasilia = new Movie("brasilia");
+            Movie snatch = new Movie("Snatch");
+
+            movieRepository.save(brasilia);
+            movieRepository.save(snatch);
+        };
+    }
 }
